ce488c91253bc9124e432a1e633d729b56fb7865
Jeremiah Wafula <jeremiahwafula01@gmail.com>
2024-09-20 08:54:03 +0300

    removed hibernate ddl file

--- src/main/java/com/crud/pet_service/model/Pet.java
+++ src/main/java/com/crud/pet_service/model/Pet.java
@@ -1,11 +1,13 @@
 package com.crud.pet_service.model;
 
 import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
 @Data
+@Transactional
 @Table(name="pets")
 public class Pet {
     @Id
