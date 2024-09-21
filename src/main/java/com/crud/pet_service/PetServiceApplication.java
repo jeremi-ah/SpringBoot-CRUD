68e73638fdd3eb42faa91e2f0c072cb2bd726b6e
Jeremiah Wafula <jeremiahwafula01@gmail.com>
2024-09-21 23:54:16 +0300

    updated petDto with modelMapper

--- src/main/java/com/crud/pet_service/PetServiceApplication.java
+++ src/main/java/com/crud/pet_service/PetServiceApplication.java
@@ -1,10 +1,18 @@
 package com.crud.pet_service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PetServiceApplication {
+
+	//using model mapper for dto
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetServiceApplication.class, args);
--- src/main/java/com/crud/pet_service/service/PetServiceImpl.java
+++ src/main/java/com/crud/pet_service/service/PetServiceImpl.java
@@ -4,6 +4,7 @@ import com.crud.pet_service.dto.PetDto;
 import com.crud.pet_service.model.Pet;
 import com.crud.pet_service.repository.PetRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -14,14 +15,23 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class PetServiceImpl implements PetService {
     private PetRepository petRepository;
+    private ModelMapper modelMapper;
 
     //function converting Pet Entity to dto
+//    private PetDto convertToDto(Pet pet){
+//        PetDto petDto = new PetDto();
+//        petDto.setAge(pet.getAge());
+//        petDto.setName(pet.getName());
+//        petDto.setColor(pet.getColor());
+//        petDto.setSpecies(pet.getSpecies());
+//        return petDto;
+//    }
+
+
+    //using modelMapper
     private PetDto convertToDto(Pet pet){
         PetDto petDto = new PetDto();
-        petDto.setAge(pet.getAge());
-        petDto.setName(pet.getName());
-        petDto.setColor(pet.getColor());
-        petDto.setSpecies(pet.getSpecies());
+        petDto=modelMapper.map(pet,PetDto.class);
         return petDto;
     }
 
